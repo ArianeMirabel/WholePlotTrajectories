49530908bb9c1c0ddcb4a95e0afc269c50114c3f
Eric Marcon <eric.marcon@ecofog.gf>
2018-01-26 19:45:50 -0300

    Script GithubPages
    Met à jour les pages web du projet

---
+++ GithubPages.R
@@ -0,0 +1,38 @@
+### Make github pages (master branch / docs as web site)
+# The README.md file is copied to /docs. Make a link to the main html file in it
+# html files, including figures and libs are copied to /docs
+# pdf files are copied to /docs
+# Source this file before committing
+
+# Create the docs folder
+dir.create("docs")
+
+# Copy knitted html files
+htmlFiles <- list.files(pattern="*.html")
+if (length(htmlFiles) > 0)
+  file.rename(from=htmlFiles, to=paste("docs/", htmlFiles, sep=""))
+# Figures
+html_filesDir <- list.files(pattern="*_files")
+if (length(html_filesDir) > 0) {
+  dir.create(paste("docs/", html_filesDir, sep=""))
+  dir.create(paste("docs/", html_filesDir, "/figure-html", sep=""))
+  html_files <- list.files(html_filesDir, full.names = TRUE, recursive=TRUE)
+  if (length(html_files) > 0)
+    file.copy(from=html_files, to=paste("docs/", html_files, sep = ""))
+}
+#libs
+dir.create("docs/libs")
+libsDirs <- list.files(path="libs")
+if (length(libsDirs) > 0) {
+  sapply(paste("docs/libs/", libsDirs, sep = ""), dir.create)
+  libsFiles <- list.files("libs", full.names = TRUE, recursive=TRUE)
+  file.copy(from=libsFiles, to=paste("docs/", libsFiles, sep = ""))
+}
+
+# Copy knitted pdf files
+pdfFiles <- list.files(pattern="*.pdf")
+if (length(pdfFiles) > 0)
+  file.rename(from=pdfFiles, to=paste("docs/", pdfFiles, sep=""))
+
+# Copy README.md to docs
+file.copy(from="README.md", to="docs/README.md")
